67873809ad491e8a7a8e7b2e39f2a91871b1c8cf
efbar <17676301+efbar@users.noreply.github.com>
2020-06-11 18:04:58 +0200

    add output module

---
+++ output.tf
@@ -0,0 +1,9 @@
+output "private_ip-worker-0" {
+  value = google_compute_instance.hcpoc_workers[0].network_interface.0.network_ip
+}
+output "private_ip-worker-1" {
+  value = google_compute_instance.hcpoc_workers[1].network_interface.0.network_ip
+}
+output "private_ip-worker-2" {
+  value = google_compute_instance.hcpoc_workers[2].network_interface.0.network_ip
+}
