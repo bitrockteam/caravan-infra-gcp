981fe838427d3278b2cedf0003f88d2a93318316
efbar <17676301+efbar@users.noreply.github.com>
2020-06-11 16:37:48 +0200

    add packer module

---
+++ bootstrap.tf
@@ -0,0 +1,10 @@
+provider "google" {
+  region = var.region
+}
+
+module "packer_build" {
+  source                      = "git::ssh://git@github.com/bitrockteam/hcpoc-base-packer-centos?ref=master"
+  build_on_google             = true
+  google_project_id           = var.project_id
+  google_account_file         = var.google_account_file
+}
