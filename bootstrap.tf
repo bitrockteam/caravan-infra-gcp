6410000803a2e68d8ec09f1768b60c38a1ca56d0
efbar <17676301+efbar@users.noreply.github.com>
2020-06-11 18:04:01 +0200

    format

--- bootstrap.tf
+++ bootstrap.tf
@@ -3,8 +3,8 @@ provider "google" {
 }
 
 module "packer_build" {
-  source                      = "git::ssh://git@github.com/bitrockteam/hcpoc-base-packer-centos?ref=master"
-  build_on_google             = true
-  google_project_id           = var.project_id
-  google_account_file         = var.google_account_file
+  source              = "git::ssh://git@github.com/bitrockteam/hcpoc-base-packer-centos?ref=master"
+  build_on_google     = true
+  google_project_id   = var.project_id
+  google_account_file = var.google_account_file
 }
