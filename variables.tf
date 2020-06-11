b62997e6035f7e1189761184ec40e7c2b1ff918d
efbar <17676301+efbar@users.noreply.github.com>
2020-06-11 18:05:28 +0200

    add variables module

--- variables.tf
+++ variables.tf
@@ -4,3 +4,14 @@ variable "org_id" {}
 variable "region" {}
 variable "google_account_file" {}
 variable "project_id" {}
+variable "instance_count" {
+  default = "3"
+}
+variable "prefix" {
+  description = "The prefix of the objects' names"
+  default     = "hcpoc-gcp"
+}
+variable "subnet_prefix" {
+  description = "The address prefix to use for the subnet."
+  default     = "10.128.0.0/28"
+}
