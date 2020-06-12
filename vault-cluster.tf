91a2f94812692d7b8237c71ba897b3bb180a2b0f
efbar <17676301+efbar@users.noreply.github.com>
2020-06-12 12:55:39 +0200

    add vault module

---
+++ vault-cluster.tf
@@ -0,0 +1,8 @@
+module "vault_cluster" {
+  source                   = "git::ssh://git@github.com/bitrockteam/hcpoc-base-vault-baseline//modules/cluster-raft?ref=master"
+  unseal_keyring           = google_kms_key_ring.vault_keyring.name
+  unseal_key               = google_kms_crypto_key.vault_key.name
+  cluster_nodes            = { for n in google_compute_instance.hcpoc_cluster_nodes : n.name => n.name }
+  cluster_nodes_public_ips = { for n in google_compute_instance.hcpoc_cluster_nodes : n.name => n.network_interface.0.access_config.0.nat_ip }
+  ssh_private_key          = chomp(tls_private_key.ssh-key.private_key_pem)
+}
