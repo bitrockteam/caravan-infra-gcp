2bb75fec6e6818106acd7438be69d4eee02d6b99
efbar <17676301+efbar@users.noreply.github.com>
2020-06-12 17:31:37 +0200

    set self_link

--- kms.tf
+++ kms.tf
@@ -21,7 +21,7 @@ resource "google_kms_key_ring_iam_binding" "vault_iam_kms_binding" {
 
 resource "google_kms_crypto_key" "vault_key" {
   name            = "vault-crypto-key"
-  key_ring        = google_kms_key_ring.vault_keyring.id
+  key_ring        = google_kms_key_ring.vault_keyring.self_link
   rotation_period = "1209660s"
 
   lifecycle {
