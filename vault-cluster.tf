4f801ac3d71abc2af617d05540c4c3e6af7039a2
efbar <17676301+efbar@users.noreply.github.com>
2020-06-15 12:57:40 +0200

    add unseal_project_id var

--- vault-cluster.tf
+++ vault-cluster.tf
@@ -1,6 +1,6 @@
 module "vault_cluster" {
   source                   = "git::ssh://git@github.com/bitrockteam/hcpoc-base-vault-baseline//modules/cluster-raft?ref=master"
-  # project_id               = var.project_id
+  unseal_project_id               = var.project_id
   unseal_keyring           = google_kms_key_ring.vault_keyring.name
   unseal_key               = google_kms_crypto_key.vault_key.name
   cluster_nodes            = { for n in google_compute_instance.hcpoc_cluster_nodes : n.name => n.network_interface.0.network_ip }
