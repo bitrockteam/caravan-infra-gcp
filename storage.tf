cc78e7e5197455778060535b5d02e4bc944867e3
efbar <17676301+efbar@users.noreply.github.com>
2020-06-15 18:15:49 +0200

    remove bucket

--- storage.tf
+++
@@ -1,7 +0,0 @@
-resource "google_storage_bucket" "hcpoc" {
-  project       = var.project_id
-  name          = "${var.prefix}-storage"
-  location      = "us-central1"
-  force_destroy = true
-  storage_class = "REGIONAL"
-}
