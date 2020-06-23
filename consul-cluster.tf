e49e69d2830d9ebeed1bc30a1fe5834343d41b4c
gfdsa <mtabolsky@gmail.com>
2020-06-23 09:45:49 +0200

    Better triggers

--- consul-cluster.tf
+++ consul-cluster.tf
@@ -1,6 +1,8 @@
 module "consul-cluster" {
-  source                    = "git::ssh://git@github.com/bitrockteam/hcpoc-base-consul-baseline//modules/consul-cluster?ref=master"
-  ssh_private_key           = chomp(tls_private_key.ssh-key.private_key_pem)
-  cluster_nodes             = { for n in google_compute_instance.hcpoc_cluster_nodes : n.name => n.network_interface.0.network_ip }
-  cluster_nodes_public_ips  = { for n in google_compute_instance.hcpoc_cluster_nodes : n.name => n.network_interface.0.access_config.0.nat_ip }
+  source                   = "git::ssh://git@github.com/bitrockteam/hcpoc-base-consul-baseline//modules/consul-cluster?ref=master"
+  ssh_private_key          = chomp(tls_private_key.ssh-key.private_key_pem)
+  cluster_nodes_ids        = google_compute_instance.hcpoc_cluster_nodes[*].instance_id
+  cluster_nodes            = { for n in google_compute_instance.hcpoc_cluster_nodes : n.name => n.network_interface.0.network_ip }
+  cluster_nodes_public_ips = { for n in google_compute_instance.hcpoc_cluster_nodes : n.name => n.network_interface.0.access_config.0.nat_ip }
+  vault_address            = module.vault_cluster.vault_address
 }
--- vault-cluster.tf
+++ vault-cluster.tf
@@ -3,6 +3,7 @@ module "vault_cluster" {
   unseal_project_id        = var.project_id
   unseal_keyring           = google_kms_key_ring.vault_keyring.name
   unseal_key               = google_kms_crypto_key.vault_key.name
+  cluster_nodes_ids        = google_compute_instance.hcpoc_cluster_nodes[*].instance_id
   cluster_nodes            = { for n in google_compute_instance.hcpoc_cluster_nodes : n.name => n.network_interface.0.network_ip }
   cluster_nodes_public_ips = { for n in google_compute_instance.hcpoc_cluster_nodes : n.name => n.network_interface.0.access_config.0.nat_ip }
   ssh_private_key          = chomp(tls_private_key.ssh-key.private_key_pem)
