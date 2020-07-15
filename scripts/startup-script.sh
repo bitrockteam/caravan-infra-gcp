432635cf97660298f2f9f191ad0c253046006288
efbar <17676301+efbar@users.noreply.github.com>
2020-07-15 19:45:37 +0200

    decrease waiting time

--- scripts/startup-script.sh
+++ scripts/startup-script.sh
@@ -3,7 +3,7 @@
 sudo service stackdriver-agent start
 
 if [[ `hostname` != clusternode* ]]; then
-    sleep 100s && \
+    sleep 60s && \
     export TOKEN=$(curl -s "http://metadata.google.internal/computeMetadata/v1/instance/service-accounts/default/token"  -H "Metadata-Flavor: Google" | grep -Po '(?<="access_token":")([^"]*)') \
         VAULT_AGENT_CONFIG=$(curl -s -H 'Metadata-Flavor: Google' http://metadata.google.internal/computeMetadata/v1/instance/attributes/vault-agent-config) \
         CONSUL_AGENT_CONFIG=$(curl -s -H 'Metadata-Flavor: Google' http://metadata.google.internal/computeMetadata/v1/instance/attributes/consul-agent-config)
