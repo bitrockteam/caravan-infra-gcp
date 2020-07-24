76e7fc7a809c3a233ce9bc509efa63637c82fdba
efbar <17676301+efbar@users.noreply.github.com>
2020-07-24 11:10:25 +0200

    no more needed

--- nomad-cluster.tf
+++ nomad-cluster.tf
@@ -4,5 +4,4 @@ module "nomad-cluster" {
   cluster_nodes_ids        = google_compute_instance.hcpoc_cluster_nodes[*].instance_id
   cluster_nodes            = { for n in google_compute_instance.hcpoc_cluster_nodes : n.name => n.network_interface.0.network_ip }
   cluster_nodes_public_ips = { for n in google_compute_instance.hcpoc_cluster_nodes : n.name => n.network_interface.0.access_config.0.nat_ip }
-  consul_address            = module.consul-cluster.consul_address
 }
