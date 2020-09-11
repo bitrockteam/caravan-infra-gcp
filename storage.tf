d2c5374a75fcc8ea8e5142195662fff6e1fb7fde
efbar <17676301+efbar@users.noreply.github.com>
2020-09-11 10:51:35 +0200

    remove deprecated attribute

--- storage.tf
+++ storage.tf
@@ -3,7 +3,7 @@ resource "google_storage_bucket" "configs" {
   location           = var.region
   project            = var.project_id
   force_destroy      = true
-  bucket_policy_only = true
+  uniform_bucket_level_access = true
   versioning {
     enabled = true
   }
