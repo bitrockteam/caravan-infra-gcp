b9f1945dbaaf73f8408aca7f2e22f5baf81d49e0
efbar <17676301+efbar@users.noreply.github.com>
2020-09-21 09:24:37 +0200

    just align..

--- vault-cluster.tf
+++ vault-cluster.tf
@@ -9,9 +9,9 @@ module "vault_cluster" {
   ssh_private_key          = chomp(tls_private_key.ssh-key.private_key_pem)
   ssh_user                 = var.ssh_user
   ssh_timeout              = var.ssh_timeout
-  gcp_project_id      = var.project_id
-  gcp_node_role       = "cluster-node"
-  gcp_service_account = "cluster-node@${var.project_id}.iam.gserviceaccount.com"
+  gcp_project_id           = var.project_id
+  gcp_node_role            = "cluster-node"
+  gcp_service_account      = "cluster-node@${var.project_id}.iam.gserviceaccount.com"
 }
 module "vault_cluster_agents" {
   source              = "../hcpoc-base-vault-baseline/modules/agent"
@@ -21,11 +21,10 @@ module "vault_cluster_agents" {
   gcp_auto_auth       = true
   gcp_node_role       = "cluster-node"
   gcp_service_account = "cluster-node@${var.project_id}.iam.gserviceaccount.com"
-
-  nodes_ids        = google_compute_instance.hcpoc_cluster_nodes[*].instance_id
-  nodes            = { for n in google_compute_instance.hcpoc_cluster_nodes : n.name => n.network_interface.0.network_ip }
-  nodes_public_ips = { for n in google_compute_instance.hcpoc_cluster_nodes : n.name => n.network_interface.0.access_config.0.nat_ip }
-  ssh_private_key  = chomp(tls_private_key.ssh-key.private_key_pem)
-  ssh_user         = var.ssh_user
-  ssh_timeout      = var.ssh_timeout
+  nodes_ids           = google_compute_instance.hcpoc_cluster_nodes[*].instance_id
+  nodes               = { for n in google_compute_instance.hcpoc_cluster_nodes : n.name => n.network_interface.0.network_ip }
+  nodes_public_ips    = { for n in google_compute_instance.hcpoc_cluster_nodes : n.name => n.network_interface.0.access_config.0.nat_ip }
+  ssh_private_key     = chomp(tls_private_key.ssh-key.private_key_pem)
+  ssh_user            = var.ssh_user
+  ssh_timeout         = var.ssh_timeout
 }
