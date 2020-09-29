725571d6081fa409912ef295b7e6cae23d7fbc71
efbar <17676301+efbar@users.noreply.github.com>
2020-09-29 17:40:05 +0200

    no more relative path as source module

--- vault-cluster.tf
+++ vault-cluster.tf
@@ -15,7 +15,7 @@ module "vault_cluster" {
   external_domain          = var.external_domain
 }
 module "vault_cluster_agents" {
-  source              = "../hcpoc-base-vault-baseline/modules/agent"
+  source              = "git::ssh://git@github.com/bitrockteam/hcpoc-base-vault-baseline//modules/agent?ref=master"
   vault_endpoint      = "http://127.0.0.1:8200"
   tcp_listener_tls    = false
   gcp_project_id      = var.project_id
