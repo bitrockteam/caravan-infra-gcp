1aa5a73201de6b3ff4e8c7e0331db5875e00453d
efbar <17676301+efbar@users.noreply.github.com>
2020-10-10 12:54:57 +0200

    no more registry here

--- storage.tf
+++ storage.tf
@@ -9,15 +9,3 @@ resource "google_storage_bucket" "configs" {
   }
   storage_class = "REGIONAL"
 }
-
-resource "google_storage_bucket" "registry" {
-  name                        = "registry-${var.project_id}"
-  location                    = var.region
-  project                     = var.project_id
-  force_destroy               = true
-  uniform_bucket_level_access = true
-  versioning {
-    enabled = true
-  }
-  storage_class = "REGIONAL"
-}
