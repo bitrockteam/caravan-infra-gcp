6f1a7c31890c90506878378cbdee96bfdae7065d
efbar <17676301+efbar@users.noreply.github.com>
2020-10-23 12:32:04 +0200

    add output for platform tfvar

--- output.tf
+++ output.tf
@@ -1,27 +1,7 @@
-# output "private_ip-cluster-0" {
-#   value = google_compute_instance.hashicorp_cluster_nodes.length >= 1 ? google_compute_instance.hashicorp_cluster_nodes[0].network_interface.0.access_config.0.nat_ip : null
-# }
-# output "private_ip-cluster-1" {
-#   value = google_compute_instance.hashicorp_cluster_nodes.length >= 2 ? google_compute_instance.hashicorp_cluster_nodes[1].network_interface.0.access_config.0.nat_ip : null
-# }
-# output "private_ip-cluster-2" {
-#   value = google_compute_instance.hashicorp_cluster_nodes.length >= 3 ? google_compute_instance.hashicorp_cluster_nodes[2].network_interface.0.access_config.0.nat_ip : null
-# }
 output "cluster-public-ips" {
   value = {
     for n in google_compute_instance.hashicorp_cluster_nodes : n.name => n.network_interface.0.access_config.0.nat_ip
   }
-}
-output "cluster-private-ips" {
-  value = {
-    for n in google_compute_instance.hashicorp_cluster_nodes : n.name => n.network_interface.0.network_ip
-  }
-}
-output "configs-bucket-url" {
-  value = google_storage_bucket.configs.url
-}
-output "worker_nodes_service_accounts" {
-  value = [for k, v in data.google_service_account.worker_node_service_account : v.email]
 }
 output "load-balancer-ip-address" {
   value = google_compute_global_forwarding_rule.global_forwarding_rule.ip_address
@@ -35,4 +15,13 @@ output "hashicorp_endpoints" {
     consul = "https://consul.${var.prefix}.${var.external_domain}"
     nomad  = "https://nomad.${var.prefix}.${var.external_domain}"
   }
+}
+output "PROJECT_PLATFORM_TFVAR" {
+  value = templatefile("${path.module}/templates/tfvar.tmpl",
+  { 
+    project_id = var.project_id,
+    prefix = var.prefix,
+    external_domain = var.external_domain,
+    region = var.region
+  })
 }
