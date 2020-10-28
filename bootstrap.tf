8704738dcca3a210e326a4482615704f5e6202c0
gfdsa <michael.tabolsky@bitrock.it>
2020-10-28 12:35:08 +0100

    Remove branch reference for master

--- bootstrap.tf
+++ bootstrap.tf
@@ -1,5 +1,5 @@
 module "hashicorp-bootstrap" {
-  source                   = "git::ssh://git@github.com/bitrockteam/hashicorp-terraform-bootstrap?ref=nomad-acl"
+  source                   = "git::ssh://git@github.com/bitrockteam/hashicorp-terraform-bootstrap?ref=master"
   cluster_nodes_ids        = google_compute_instance.hashicorp_cluster_nodes[*].instance_id
   cluster_nodes            = { for n in google_compute_instance.hashicorp_cluster_nodes : n.name => n.network_interface.0.network_ip }
   cluster_nodes_public_ips = { for n in google_compute_instance.hashicorp_cluster_nodes : n.name => n.network_interface.0.access_config.0.nat_ip }
