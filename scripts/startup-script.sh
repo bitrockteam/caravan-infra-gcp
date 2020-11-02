01f287d3458f2ed63e5541ca164f7f2baf377862
efbar <17676301+efbar@users.noreply.github.com>
2020-11-02 17:25:22 +0100

    refactoring

--- scripts/startup-script.sh
+++ scripts/startup-script.sh
@@ -1,8 +1,12 @@
 #! /bin/bash
 
-sudo service stackdriver-agent start
 
-if [[ `hostname` != clustnode* ]]; then
+if [[ `hostname` == clustnode* ]]; then
+    sudo systemctl enable nomad-watcher.path && \
+    sudo systemctl enable nomad-watcher.service && \
+    sudo systemctl start nomad-watcher.path && \
+    sudo systemctl start nomad-watcher.service
+else
     sleep 60s && \
     export TOKEN=$(curl -s "http://metadata.google.internal/computeMetadata/v1/instance/service-accounts/default/token"  -H "Metadata-Flavor: Google" | grep -Po '(?<="access_token":")([^"]*)') \
         VAULT_AGENT_CONFIG=$(curl -s -H 'Metadata-Flavor: Google' http://metadata.google.internal/computeMetadata/v1/instance/attributes/vault-agent-config)
@@ -28,4 +32,22 @@ if [[ `hostname` != clustnode* ]]; then
     systemctl restart nomad
 fi
 
-# sudo docker login -u oauth2accesstoken -p $(echo $TOKEN) https://us.gcr.io
+if [[ `hostname` == monitoring* ]]; then
+    export ELASTIC_SERVICE=$(curl -s -H 'Metadata-Flavor: Google' http://metadata.google.internal/computeMetadata/v1/instance/attributes/elastic-service) \
+        GRAFANA_SERVICE=$(curl -s -H 'Metadata-Flavor: Google' http://metadata.google.internal/computeMetadata/v1/instance/attributes/grafana-service)
+        PROMETHEUS_SERVICE=$(curl -s -H 'Metadata-Flavor: Google' http://metadata.google.internal/computeMetadata/v1/instance/attributes/prometheus-service)
+    curl -o /etc/consul.d/elastic-service.json -s -L -H "Authorization: Bearer $TOKEN" $ELASTIC_SERVICE && \
+    curl -o /etc/consul.d/grafana-service.json -s -L -H "Authorization: Bearer $TOKEN" $GRAFANA_SERVICE && \
+    curl -o /etc/consul.d/prometheus-service.json -s -L -H "Authorization: Bearer $TOKEN" $PROMETHEUS_SERVICE && \
+    sudo sysctl -w vm.max_map_count=262144 && \
+    sudo systemctl enable prometheus && \
+    sudo systemctl start prometheus && \
+    sudo systemctl enable elasticsearch && \
+    sudo systemctl start elasticsearch && \
+    sudo systemctl enable grafana-server && \
+    sudo systemctl start grafana-server
+fi
+
+
+(crontab -l 2>/dev/null; echo "*/15 * * * * /usr/bin/systemctl restart docker-login.service") | crontab -
+sudo service stackdriver-agent start 
