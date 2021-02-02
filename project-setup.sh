d967a7da71b6e619f49d6406c5da9159bb9c9fa0
efbar <17676301+efbar@users.noreply.github.com>
2021-02-02 14:21:07 +0100

    Some fixes

--- project-setup.sh
+++ project-setup.sh
@@ -10,9 +10,9 @@ PROJECT_NAME=$5
 REGION=$6
 
 echo "Creating ${PROJECT_ID}, named ${PROJECT_NAME} in ${REGION}..."
-#gcloud projects create ${PROJECT_ID} --name=${PROJECT_NAME} --organization=${ORG_ID}
+gcloud projects create ${PROJECT_ID} --name=${PROJECT_NAME} --organization=${ORG_ID}
 gcloud config set project ${PROJECT_ID}
-#gcloud beta billing projects link ${PROJECT_ID} --billing-account ${BILLING_ACCOUNT_ID}
+gcloud beta billing projects link ${PROJECT_ID} --billing-account ${BILLING_ACCOUNT_ID}
 
 echo "Enable some GCP services..."
 gcloud services enable compute.googleapis.com \
@@ -40,7 +40,7 @@ gcloud projects add-iam-policy-binding ${PARENT_PROJECT} --member=serviceAccount
 
 gcloud projects add-iam-policy-binding ${PARENT_PROJECT} --member=serviceAccount:$(gcloud projects describe ${PROJECT_ID} --format=json | jq -r '.projectNumber')@cloudservices.gserviceaccount.com --role=roles/compute.imageUser
 
-gcloud iam service-accounts keys create .hashicorp-key.json  --iam-account terraform@${PROJECT_ID}.iam.gserviceaccount.com
+gcloud iam service-accounts keys create .${PROJECT_NAME}-key.json  --iam-account terraform@${PROJECT_ID}.iam.gserviceaccount.com
 
 echo "Write tfvars and backend files."
 cat <<EOT > gcp.tfvars
