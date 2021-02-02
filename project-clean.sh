31d97708eb297ba1e6f258823463574faa52be30
efbar <17676301+efbar@users.noreply.github.com>
2021-02-02 14:21:13 +0100

    add cleaner

---
+++ project-clean.sh
@@ -0,0 +1,18 @@
+#! /bin/bash
+
+set -ex
+
+PROJECT_ID=$1
+PARENT_PROJECT=$2
+
+echo "Clean some policy binding in "${PARENT_PROJECT}" project..."
+gcloud projects add-iam-policy-binding ${PARENT_PROJECT} --member=serviceAccount:$(gcloud projects describe ${PROJECT_ID} --format=json | jq -r '.projectNumber')@cloudservices.gserviceaccount.com --role=roles/compute.imageUser
+gcloud projects add-iam-policy-binding ${PARENT_PROJECT} --member=serviceAccount:terraform@${PROJECT_ID}.iam.gserviceaccount.com --role=roles/compute.imageUser
+gcloud projects add-iam-policy-binding ${PARENT_PROJECT} --member=serviceAccount:terraform@${PROJECT_ID}.iam.gserviceaccount.com --role=roles/dns.admin
+gcloud projects add-iam-policy-binding ${PARENT_PROJECT} --member=serviceAccount:terraform@${PROJECT_ID}.iam.gserviceaccount.com --role=roles/compute.networkAdmin
+gcloud projects add-iam-policy-binding ${PARENT_PROJECT} --member=serviceAccount:terraform@${PROJECT_ID}.iam.gserviceaccount.com --role=roles/iam.serviceAccountUser
+
+echo "Delete project ${PROJECT_ID}"
+gcloud projects delete ${PROJECT_ID}
+
+echo "Done!"
