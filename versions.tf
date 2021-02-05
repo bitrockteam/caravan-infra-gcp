d085ac1740e0c1de3da5c597dda99033387943fc
efbar <17676301+efbar@users.noreply.github.com>
2021-02-05 14:29:24 +0100

    add versions

---
+++ versions.tf
@@ -0,0 +1,23 @@
+terraform {
+  required_providers {
+    acme = {
+      source = "vancluever/acme"
+    }
+    google = {
+      source = "hashicorp/google"
+    }
+    local = {
+      source = "hashicorp/local"
+    }
+    null = {
+      source = "hashicorp/null"
+    }
+    random = {
+      source = "hashicorp/random"
+    }
+    tls = {
+      source = "hashicorp/tls"
+    }
+  }
+  required_version = ">= 0.13"
+}
