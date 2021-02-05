9b3188959c0937627aed1b532ddd07870861a83b
efbar <17676301+efbar@users.noreply.github.com>
2021-02-05 09:48:16 +0100

    add color

--- project-setup.sh
+++ project-setup.sh
@@ -9,13 +9,13 @@ PROJECT_ID=$4
 PROJECT_NAME=$5
 REGION=$6
 
-echo "Creating ${PROJECT_ID}, named ${PROJECT_NAME} in ${REGION}..."
+echo -e "Creating ${PROJECT_ID}, named ${PROJECT_NAME} in ${REGION}...\033[0m"
 gcloud projects create ${PROJECT_ID} --name=${PROJECT_NAME} --organization=${ORG_ID}
 gcloud config set project ${PROJECT_ID}
 gcloud projects add-iam-policy-binding ${PROJECT_ID} --member=user:$(gcloud config list --format=json | jq -r ".core.account") --role=roles/owner
 gcloud beta billing projects link ${PROJECT_ID} --billing-account ${BILLING_ACCOUNT_ID}
 
-echo "Enable some GCP services..."
+echo -e "\033[32mEnable some GCP services...\033[32m"
 gcloud services enable compute.googleapis.com \
     monitoring.googleapis.com \
     logging.googleapis.com \
@@ -25,10 +25,10 @@ gcloud services enable compute.googleapis.com \
     cloudresourcemanager.googleapis.com \
     dns.googleapis.com
 
-echo "Create terraform state bucket..."
+echo -e "\033[32mCreate terraform state bucket...\033[0m"
 gsutil mb -b off -c standard -l ${REGION} -p ${PROJECT_ID} gs://states-bucket-${PROJECT_ID}
 
-echo "Create terraform service account and its json key..."
+echo -e "\033[32mCreate terraform service account and its json key...\033[0m"
 gcloud iam service-accounts create terraform
 
 gcloud projects add-iam-policy-binding ${PROJECT_ID} --member=serviceAccount:terraform@${PROJECT_ID}.iam.gserviceaccount.com --role=roles/owner
@@ -42,7 +42,7 @@ gcloud projects add-iam-policy-binding ${PARENT_PROJECT_ID} --member=serviceAcco
 
 gcloud iam service-accounts keys create .${PROJECT_NAME}-key.json  --iam-account terraform@${PROJECT_ID}.iam.gserviceaccount.com
 
-echo "Write tfvars and backend files."
+echo -e "\033[32mWrite tfvars and backend files.\033[0m"
 cat <<EOT > gcp.tfvars
 region                = "${REGION}"
 zone                  = "${REGION}-a"
@@ -70,6 +70,7 @@ terraform {
 }
 EOT
 
-echo "Done!
+echo -e "\033[32m
+Done!
 Don't forget to add the service account \"terraform@${PROJECT_ID}.iam.gserviceaccount.com\" at https://www.google.com/webmasters/verification for your parent DNS zone.
-"
+\033[0m"
