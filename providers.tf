e281f8aa3c0c1b19413e3f4160b1c3705a2f78d7
Matteo Gazzetta <matteo.gazzetta@bitrock.it>
2021-02-15 18:35:44 +0100

    fix: duplcated required_version

--- providers.tf
+++ providers.tf
@@ -4,10 +4,6 @@ provider "google" {
   credentials = file(var.google_account_file)
 }
 
-terraform {
-  required_version = "~> 0.13.1"
-}
-
 provider "acme" {
   server_url = var.use_le_staging ? var.le_staging_endpoint : var.le_production_endpoint
 }
