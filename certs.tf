9ae864424057c8974db053d08f3f41d5c4be5a22
Matteo Gazzetta <matteo.gazzetta@bitrock.it>
2022-08-17 18:25:05 +0200

    Fix acme gcp

--- certs.tf
+++ certs.tf
@@ -1,13 +1,14 @@
 module "terraform-acme-le" {
-  depends_on               = [module.caravan-bootstrap]
-  source                   = "git::https://github.com/bitrockteam/caravan-acme-le?ref=refs/tags/v0.0.15"
-  gcp_project_id           = var.project_id
-  gcp_service_account_file = var.google_account_file
-  common_name              = concat([trimsuffix(google_dns_managed_zone.project-zone.dns_name, ".")], [google_dns_managed_zone.project-zone.id])[0]
-  dns_provider             = "gcp"
-  private_key              = tls_private_key.cert_private_key.private_key_pem
-  dns_propagation_timeout  = "600"
-  dns_polling_interval     = "20"
+  depends_on                   = [module.caravan-bootstrap]
+  source                       = "git::https://github.com/bitrockteam/caravan-acme-le?ref=refs/tags/v0.0.16"
+  gcp_project_id               = var.project_id
+  gcp_service_account_file     = var.google_account_file
+  common_name                  = concat([trimsuffix(google_dns_managed_zone.project-zone.dns_name, ".")], [google_dns_managed_zone.project-zone.id])[0]
+  dns_provider                 = "gcp"
+  private_key                  = tls_private_key.cert_private_key.private_key_pem
+  dns_propagation_timeout      = "600"
+  dns_polling_interval         = "20"
+  disable_complete_propagation = true
 }
 
 resource "null_resource" "ca_certs" {
